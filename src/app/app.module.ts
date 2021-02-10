0f21e839291e3b22b1e81a7091693f6fde8700ad
Aquila Sands <5774614+AquilaSands@users.noreply.github.com>
2021-02-10 20:24:15 +0000

    chore: add fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FontAwesomeModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
