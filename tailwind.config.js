be972846a481af4e0cb6324f01f09c73f0c6bd0f
Aquila Sands <5774614+AquilaSands@users.noreply.github.com>
2021-04-24 15:40:12 +0100

    chore: update angular and packages
    Angular now has built in support for TailwindCSS.
    This is used instead of the custom webpack approach.
    There was an issue where Angular was not compatable with NPM 7.
    See https://github.com/angular/angular-cli/issues/19957
    This version of Angular works with NPM > 7.5.6.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.ts'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	prefix: '',
+	purge: {
+		enabled: process.env.NODE_ENV === 'production',
+		mode: 'all',
+		content: ['./src/**/*.{html,ts}'],
+	},
+	darkMode: 'class', // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
